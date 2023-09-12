4ddbf083017e441e478452556d1ae881a8c1dfb4
Khoa-bit <nguyenanhkhoablue@proton.me>
2023-09-12 23:18:09 +0700

    complete: day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -10,7 +10,7 @@ import (
 //go:embed input.txt
 var input string
 
-func main() {
+func part1(input string) int {
 	trim_input := strings.Trim(input, "\r\n")
 
 	max := 0
@@ -22,7 +22,7 @@ func main() {
 			cal_int, err := strconv.Atoi(cal)
 			if err != nil {
 				fmt.Println(err)
-				return
+				return -1
 			}
 
 			total += cal_int
@@ -33,5 +33,48 @@ func main() {
 		}
 	}
 
-	fmt.Println(max)
+	return max
+}
+
+func part2(input string) int {
+	trim_input := strings.Trim(input, "\r\n")
+
+	max := make([]int, 3)
+	elfs := strings.Split(trim_input, "\r\n\r\n")
+	for _, elf := range elfs {
+		foods := strings.Split(elf, "\r\n")
+		total := 0
+		for _, cal := range foods {
+			cal_int, err := strconv.Atoi(cal)
+			if err != nil {
+				fmt.Println(err)
+				return -1
+			}
+
+			total += cal_int
+		}
+
+		cur := total
+		for i, val := range max {
+			if cur > val {
+				max[i] = cur
+				cur = val
+			}
+		}
+	}
+
+	total := 0
+	for _, val := range max {
+		total += val
+	}
+
+	return total
+}
+
+func main() {
+	result_1 := part1(input)
+	result_2 := part2(input)
+
+	fmt.Println(result_1)
+	fmt.Println(result_2)
 }
